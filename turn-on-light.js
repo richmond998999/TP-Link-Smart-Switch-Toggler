9a9f6893089310e856cf0b51226c029ca12b8ed6
Richmond Lim <richmond998999@yahoo.com>
2018-01-09 22:26:43 -0800

    Implement getToken, getDeviceID and toggleDevice

--- turn-on-light.js
+++ turn-on-light.js
@@ -1,23 +1,21 @@
 //Turn on TP-Link Lightswitch
 
-
+//Get Token using credentials
 function getToken() {
-	var authenticate_url = "https://wap.tplinkcloud.com";
-	var content_type = "application/json";
-
 
 	var message = '{"method": "login",' +
 	'"params": {' +
 	 	'"appType": "Kasa_Android",' +
 	 	'"cloudUserName": "' + username + '",' +
 	 	'"cloudPassword": "' + password + '",' +
-	 	'"terminalUUID": "MY_UUID_v4"' +
+	 	'"terminalUUID": "7a027d9c-0089-4caa-a512-a468d3a70fc3"' +
 	 	'}' +
 	'}';
 
-
+	var url = "https://wap.tplinkcloud.com";
+	var content_type = "application/json";
 	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-	xmlhttp.open("POST", authenticate_url, false);
+	xmlhttp.open("POST", url, false);
 	xmlhttp.setRequestHeader("Content-Type", "application/json");
 	xmlhttp.send(message);
 
@@ -25,5 +23,42 @@ function getToken() {
 	return result.result.token;
 }
 
+//Look for the first device's deviceId
+function getDeviceID(token) {
+
+	var message = '{"method":"getDeviceList"}';
+	var url = "https://wap.tplinkcloud.com/?token=" + token
+	var content_type = "application/json";
+	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+	xmlhttp.open("POST", url, false);
+	xmlhttp.setRequestHeader("Content-Type", "application/json");
+	xmlhttp.send(message);
+
+	var result = JSON.parse(xmlhttp.responseText);
+	return result.result.deviceList[0].deviceId;
+}
+
+//Set the device using state
+function toggleDevice(token, deviceId, state) {
+
+	var message = '{"method":"passthrough", "params": {"deviceId": "'+ deviceId +'", "requestData": "{\\"system\\": {\\"set_relay_state\\":{\\"state\\":' + state + '}}}" }}'
+
+	var url = "https://use1-wap.tplinkcloud.com/?token=" + token
+	var content_type = "application/json";
+	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+	xmlhttp.open("POST", url, false);
+	xmlhttp.setRequestHeader("Content-Type", "application/json");
+	xmlhttp.send(message);
+
+	return xmlhttp.responseText;
+}
+
 //Get Token
-var output = getToken();
+var token = getToken();
+
+//Get Device ID
+var deviceId = getDeviceID(token);
+
+//Switch the device
+var output = toggleDevice(token, deviceId, state);
+
